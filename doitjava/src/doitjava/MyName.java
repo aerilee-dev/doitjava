2f88a842abe44b5f1889bff4b48888a4bf54c3bc
404-01 <404-01@DESKTOP-U7RTCHR>
2020-08-11 14:55:36 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/MyName.java
@@ -0,0 +1,29 @@
+/**
+	*(asterisk)를 2개 사용한 주석은
+	*도큐먼터리 주석으로 문서의 정보를
+	*나타낼 때 사용합니다.
+	*작성자 : 서준호
+	*작성일 : 8월 10일
+*/
+
+//한 줄 주석입니다.
+//이것은 주석이 아니라서 얘외(에러)를 발생시킵니다.
+//주석은 컴파일할 때 삭제되어서 컴파일 됩니다.
+//그래서 주석은 프로그램의 용량에 영향을 주지 않습니다.
+/*한 번에 
+  많은 주석을 
+  넣을 때는 
+  이 주석을 
+  사용합니다.*/
+
+package doitjava;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("제 이름은 서준호입니다.");//화면출력 코드
+		System.out.println("제 직업은 강사입니다.");//둘째 줄의 출력 코드
+	}
+
+}
