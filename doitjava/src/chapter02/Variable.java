44ffa7d60026841188852e40e4999f1a1a4802e0
leeaeri <aerilee.dev@gmail.com>
2020-08-13 13:47:48 +0900

    레벨 테스트

--- doitjava/src/chapter02/Variable.java
+++ doitjava/src/chapter02/Variable.java
@@ -13,7 +13,7 @@ public class Variable {
 		System.out.println("적으로 부터 100의 데미지를 입었습니다.");
 		hp=400;
 		System.out.println("현재 체력 게이지는 얼마인가요?");
-		System.out.println(hp);
+		System.out.println(hp); 
 	}
 
 }
