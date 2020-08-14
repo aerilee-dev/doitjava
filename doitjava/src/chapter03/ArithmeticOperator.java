0cf9c63749e3bb295a7f4a096142b8ef49923004
leeaeri <aerilee.dev@gmail.com>
2020-08-14 15:43:06 +0900

    chapter03 - oprerator_2

---
+++ doitjava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int intRes;
+		double dRes;
+		intRes = 3 + 2;
+		System.out.println(intRes);
+		intRes = 3 * 2;
+		System.out.println(intRes);
+		intRes = 3 / 2;
+		System.out.println(intRes);
+		dRes = 3.0 /2.0;
+		System.out.println(dRes);
+		intRes = 3 % 2;
+		System.out.println(intRes);
+		dRes = (double)3/2;
+		System.out.println(dRes);
+		
+		
+		
+		
+	}
+
+}
