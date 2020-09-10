21c6fa86c662ff62236117ac9faee37a883c6b0a
aerilee-dev <aerilee.dev@gmail.com>
2020-09-10 13:27:45 +0900

    중복 생성자

--- doitjava/src/chapter05/Student.java
+++ doitjava/src/chapter05/Student.java
@@ -5,7 +5,29 @@ public class Student {
 	String studentName;
 	int grade;
 	String address;
+	/*생성자 - 객체를 생성할 때 실행하는 블록*/
 	
+	Student(int num1,long num2) {	//매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다."+ num1);
+	}
+	Student(int num1,int num2) {	//매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다."+ num1);
+	}
+	Student(long num1,int num2) {	//매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다."+ num1);
+	}
+	Student(int num) {	//매개변수가 없는 생성자
+		System.out.println("Student 객체를 생성합니다."+ num);
+	}	
+//	Student(){}	//디폴트 생성자는 매개변수가 없고 실행 코드가 없는 생성자
+
+Student(){
+	System.out.println("디폴트 생성자로 Student 객체를 생성합니다.");
+}	
+Student(char num){
+	System.out.println(num);
+}
+	//	메소드(메서드) - 멤버함수
 	public void showStudentInfo() { 
 		System.out.println(studentName + "," + address);
 	}
--- doitjava/src/chapter05/person.java
+++ doitjava/src/chapter05/person.java
@@ -6,4 +6,21 @@ public class person {
 	double weight;
 	char gender;
 	boolean married;
+	public person(String name, int height, double weight, char gender, boolean married) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+		this.gender = gender;
+		this.married = married;
+	}
+	public person() {
+		super();
+		this.name = "ȫ�浿";
+		this.height = 180;
+		this.weight = 75.5;
+		this.gender = 'M';
+		this.married = false;
+	}
+	
 }
