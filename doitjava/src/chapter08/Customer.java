81a31b5d42f8386aad465b6fc461f439c97ccf8b
aerilee-dev <aerilee.dev@gmail.com>
2020-09-14 15:20:17 +0900

    VIPCustomer FruitMixer

--- doitjava/src/chapter08/Customer.java
+++ doitjava/src/chapter08/Customer.java
@@ -10,10 +10,28 @@ public class Customer {
 	
 	public Customer()
 	{
+		initCustomer();
+	}
+	
+	public Customer(int customerID, String customerName){
+		this.customerID = customerID;
+		this.customerName = customerName;
+		initCustomer();
+	}	
+		private void initCustomer() {
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
 	}
 	
+	public int calcPrice(int price){
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo(){
+		return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";  
+	}
+
 	public int getCustomerID() {
 		return customerID;
 	}
@@ -36,22 +54,6 @@ public class Customer {
 
 	public void setCustomerGrade(String customerGrade) {
 		this.customerGrade = customerGrade;
-	}
-
-	public Customer(int customerID, String customerName){
-		this.customerID = customerID;
-		this.customerName = customerName;
-		customerGrade = "SILVER";
-		bonusRatio = 0.01;
-	}
-	
-	public int calcPrice(int price){
-		bonusPoint += price * bonusRatio;
-		return price;
-	}
-	
-	public String showCustomerInfo(){
-		return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";  
 	}
 	
 }
